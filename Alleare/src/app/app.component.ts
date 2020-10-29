ad75c8a8cbefa0d13e61303113df974bbb3af4ae
Zapa-Mike <zapa.mike@yahoo.com>
2020-10-29 12:10:13 +0100

    -excluded navbar from home screen

--- Alleare/src/app/app.component.ts
+++ Alleare/src/app/app.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { LocationStrategy } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  public showNavbar = true;
   title = 'Alleare';
+  constructor(private route: LocationStrategy) {}
+  ngOnInit() {
+    const route = this.route.path();
+    if (route.startsWith('/home') || route == "/") {
+      this.showNavbar = false;
+    } else {
+      this.showNavbar = true;
+    }
+  }
 }
