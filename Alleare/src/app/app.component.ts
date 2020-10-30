e34c9ae461f81656d0cd122e46eb253af346e657
tobiasb201 <49474146+tobiasb201@users.noreply.github.com>
2020-10-30 09:59:54 +0100

    Navbar and Header Control

--- Alleare/src/app/app.component.ts
+++ Alleare/src/app/app.component.ts
@@ -8,14 +8,21 @@ import { Component, DoCheck } from '@angular/core';
 })
 export class AppComponent implements DoCheck {
   public showNavbar = true;
+  public showheader = true;
   title = 'Alleare';
   constructor(private route: LocationStrategy) {}
   ngDoCheck() {
+    // Controls Navbar and Header on all screens
     const route = this.route.path();
-    if (route.startsWith('/home') || route == '/' || route == '/intro') {
+    if (route.startsWith('/home')) {
       this.showNavbar = false;
+      this.showheader = true;
+    } else if (route.startsWith('/intro')) {
+      this.showNavbar = false;
+      this.showheader = false;
     } else {
       this.showNavbar = true;
+      this.showheader = true;
     }
   }
 }
