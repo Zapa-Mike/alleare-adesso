bfdf4595b33384540513dcaca1c62b36b1efa2c6
tobiasb201 <49474146+tobiasb201@users.noreply.github.com>
2020-11-02 16:07:03 +0100

    Name into DB and output 2

--- Alleare/src/app/intro/intro.component.ts
+++ Alleare/src/app/intro/intro.component.ts
@@ -1,15 +1,44 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit ,Input} from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
+import firebase from 'firebase';
+import { stringify } from 'querystring';
 
 @Component({
   selector: 'app-intro',
   templateUrl: './intro.component.html',
   styleUrls: ['./intro.component.css']
 })
-export class IntroComponent implements OnInit {
+export class IntroComponent {
 
-  constructor() { }
+form:FormGroup;
+public Name:string;
 
-  ngOnInit(): void {
+  constructor() { 
+    this.form= new FormGroup({
+      Name: new FormControl()
+      
+    })
+
+    firebase.firestore().collection("Benutzer")
+  .onSnapshot({ includeMetadataChanges: true }, function(snapshot) {
+      snapshot.docChanges().forEach(function(change) {
+          if (change.type === "added") {
+              console.log("Name: ", change.doc.data());
+          }
+
+          var source = snapshot.metadata.fromCache ? "local cache" : "server";
+          console.log("Data came from " + source);
+      });
+  });
+
   }
+  saveName(){
+    
+    this.Name=this.form.value.Name;
+  firebase.firestore().collection('Benutzer').doc(localStorage.getItem('hans')).set({
+        Name: this.Name,
+      })
+  }
+
 
 }
