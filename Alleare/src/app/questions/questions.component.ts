114d41055e02382a3d6ee0cf74947adf5014db6b
JuliaGottschall <julia.gottschall@stud.hshl.de>
2020-11-11 14:27:35 +0100

    Story template

--- Alleare/src/app/questions/questions.component.ts
+++ Alleare/src/app/questions/questions.component.ts
@@ -1,110 +1,117 @@
-import { Component, OnInit ,Input, Output} from '@angular/core';
+import { Component, OnInit, Input, Output } from '@angular/core';
 import firebase from 'firebase';
 import { Observable } from 'rxjs';
-
 
 @Component({
   selector: 'app-questions',
   templateUrl: './questions.component.html',
-  styleUrls: ['./questions.component.css']
+  styleUrls: ['./questions.component.css'],
 })
-
-
 export class QuestionsComponent implements OnInit {
-  Fragenliste:string[]=[]; //Anderen Fragen
-  Storyliste: string[]=[]; //Alle Stories für Radio Buttons
-  StorybezogeneFragenliste: string[]=[]; //Fragen zu den Stories
-  RadiobuttonFragen: string[]=[]; //Radio Fragen ohne Story
- 
+  Fragenliste: string[] = []; //Anderen Fragen
+  Storyliste: string[] = []; //Alle Stories für Radio Buttons
+  StorybezogeneFragenliste: string[] = []; //Fragen zu den Stories
+  RadiobuttonFragen: string[] = []; //Radio Fragen ohne Story
 
   constructor() {
-
     var db = firebase.firestore();
-    var questions: string[] =[];
-    var stories : string[]= [];
+    var questions: string[] = [];
+    var stories: string[] = [];
 
     //Bundesland-ListenAbfrage
     $(document).ready(function () {
       $('#drop a').on('click', function () {
-        var txt= ($(this).text());
-        firebase.firestore().collection('Benutzer').doc(localStorage.getItem('hans')).collection('Fragenkatalog').doc('Antworten').update({
-          Frage13: txt,
-        })
+        var txt = $(this).text();
+        firebase
+          .firestore()
+          .collection('Benutzer')
+          .doc(localStorage.getItem('hans'))
+          .collection('Fragenkatalog')
+          .doc('Antworten')
+          .update({
+            Frage13: txt,
+          });
       });
     });
 
-
-    firebase.firestore().collection("Fragenkatalog")
+    firebase
+      .firestore()
+      .collection('Fragenkatalog')
       .onSnapshot({ includeMetadataChanges: true }, function (snapshot) {
         snapshot.docChanges().forEach(function (change) {
-          if (change.type === "added") {
-            console.log("Name: ", change.doc.data());
+          if (change.type === 'added') {
           }
 
-          var source = snapshot.metadata.fromCache ? "local cache" : "server";
-          console.log("Data came from " + source);
+          var source = snapshot.metadata.fromCache ? 'local cache' : 'server';
         });
       });
-      firebase.firestore().collection("_Fragenkatalog")
+    firebase
+      .firestore()
+      .collection('_Fragenkatalog')
       .onSnapshot({ includeMetadataChanges: true }, function (snapshot) {
         snapshot.docChanges().forEach(function (change) {
-          if (change.type === "added") {
-            console.log("Name: ", change.doc.data());
+          if (change.type === 'added') {
+            console.log('Name: ', change.doc.data());
           }
 
-          var source = snapshot.metadata.fromCache ? "local cache" : "server";
-          console.log("Data came from " + source);
+          var source = snapshot.metadata.fromCache ? 'local cache' : 'server';
+          console.log('Data came from ' + source);
         });
       });
 
     //Fragen, welche nicht die Stories betreffen, von der Datenbank abgreifen und umwandeln in ein String
     for (let i = 1; i <= 7; i++) {
-      db.collection("Fragenkatalog").doc("Frage" + i.toString().padStart(2, '0'))
+      db.collection('Fragenkatalog')
+        .doc('Frage' + i.toString().padStart(2, '0'))
         .withConverter(questionConverter)
-        .get().then(function (doc) {
+        .get()
+        .then(function (doc) {
           if (doc.exists) {
             questions.push(doc.data().Question);
-            tmpFragen.push(questions[i-1]);
-            if((i == 3) || (i == 5))
-              tmpRadioFragen.push(doc.data().Question);
+            tmpFragen.push(questions[i - 1]);
+            if (i == 3 || i == 5) tmpRadioFragen.push(doc.data().Question);
           } else {
-            console.log("No such document!")
+            console.log('No such document!');
           }
-        }
-        ).catch(function (error) {
-          console.log("Error getting document:", error)
+        })
+        .catch(function (error) {
+          console.log('Error getting document:', error);
         });
     }
     //Storybezogene Fragen von der Datenbank abgreifen und umwandeln in ein String
     for (let i = 1; i <= 6; i++) {
-      db.collection("_Fragenkatalog").doc("Frage" + i.toString().padStart(2, '0'))
+      db.collection('_Fragenkatalog')
+        .doc('Frage' + i.toString().padStart(2, '0'))
         .withConverter(questionConverter)
-        .get().then(function (doc) {
+        .get()
+        .then(function (doc) {
           if (doc.exists) {
             tmpStorybezogeneFragen.push(doc.data().Question);
           } else {
-            console.log("No such document!")
+            console.log('No such document!');
           }
-        }
-        ).catch(function (error) {
-          console.log("Error getting document:", error)
+        })
+        .catch(function (error) {
+          console.log('Error getting document:', error);
         });
     }
-    
+
     //Stories von der Datenbank abgreifen und umwandeln in ein String
     for (let i = 1; i <= 6; i++) {
-      db.collection("_Fragenkatalog").doc("Story" + i.toString().padStart(2, '0'))
+      db.collection('_Fragenkatalog')
+        .doc('Story' + i.toString().padStart(2, '0'))
         .withConverter(storyConverter)
-        .get().then(function (doc) {
+        .get()
+        .then(function (doc) {
           if (doc.exists) {
             stories.push(doc.data().Story);
-            tmpStories.push(stories[i-1]);
+            tmpStories.push(stories[i - 1]);
           } else {
-            console.log("No such document!")
+            console.log('No such document!');
           }
-        }
-        ).catch(function (error) {
-          console.log("Error getting document:", error)
+        })
+        .catch(function (error) {
+          console.log('Error getting document:', error);
         });
     }
     this.Fragenliste = tmpFragen;
@@ -114,25 +121,34 @@ export class QuestionsComponent implements OnInit {
   }
 
   ngOnInit() {
-      var docRef = firebase.firestore().collection('Benutzer').doc(localStorage.getItem('hans')).collection('Fragenkatalog').doc('Antworten')
-      docRef.get().then((doc) => {
+    var docRef = firebase
+      .firestore()
+      .collection('Benutzer')
+      .doc(localStorage.getItem('hans'))
+      .collection('Fragenkatalog')
+      .doc('Antworten');
+    docRef.get().then((doc) => {
       if (doc.exists) {
-        console.log("klappt");
       } else {
         // doc.data() will be undefined in this case
-        firebase.firestore().collection('Benutzer').doc(localStorage.getItem('hans')).collection('Fragenkatalog').doc('Antworten').set({
-          Initialisierung: true,
+        firebase
+          .firestore()
+          .collection('Benutzer')
+          .doc(localStorage.getItem('hans'))
+          .collection('Fragenkatalog')
+          .doc('Antworten')
+          .set({
+            Initialisierung: true,
+          });
       }
-        )}
     });
   }
 }
 
-let tmpFragen: string[]=[];
-let tmpRadioFragen: string[]=[];
-let tmpStorybezogeneFragen: string []=[];
-let tmpStories: string[]=[]; 
-
+let tmpFragen: string[] = [];
+let tmpRadioFragen: string[] = [];
+let tmpStorybezogeneFragen: string[] = [];
+let tmpStories: string[] = [];
 
 export class Questions {
   Question: string;
@@ -142,19 +158,18 @@ export class Questions {
   }
 }
 
-
 // Firestore data converter
 var questionConverter = {
   toFirestore: function (questions) {
     return {
-      _: questions.name
-    }
+      _: questions.name,
+    };
   },
   fromFirestore: function (snapshot, options) {
     const data = snapshot.data(options);
-    return new Questions(data._)
-  }
-}
+    return new Questions(data._);
+  },
+};
 
 export class Stories {
   Story: string;
@@ -168,13 +183,11 @@ export class Stories {
 var storyConverter = {
   toFirestore: function (story) {
     return {
-      _: story.name
-    }
+      _: story.name,
+    };
   },
   fromFirestore: function (snapshot, options) {
     const data = snapshot.data(options);
-    return new Stories(data._)
-  }
-}
-
-
+    return new Stories(data._);
+  },
+};
--- Alleare/src/app/questions/stories.component.ts
+++ Alleare/src/app/questions/stories.component.ts
@@ -1,16 +1,123 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, DoCheck, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-stories',
-  template: ``,
-  styleUrls: ['./questions.component.css']
-})
-export class StoriesComponent implements OnInit {
+  template: `
+  
+  
+    <div class="Frage">
+        <div class="card">
+          <div>
+            <div class="form-check form-check-inline">
+              <img src="{{imgArray[i]}}" class="img-responsive" >
+            </div>
+            <div class="ImageStory">
+            {{ storyanzeige}}
+        </div>
+            <input type="radio" id="auto1" name="auto" />
+            <label class="form-check-label"> ja </label>
+            <input type="radio" id="auto2" name="auto" />
+            <label class="form-check-label"> nein </label>
+          </div>
+        </div>
+        <div class="col rowVZ">
+          <button id="Vbutton" class="btn" (click)="push()">
+            <img
+              src="/assets/icons/icon_arrow_forward.svg"
+              width="50"
+              height="50"
+            />
+          </button>
+          <button id="Zbutton" class="btn" (click)="zurueck()">
+            <img src="/assets/icons/icon_arrow_back.svg" width="50" height="50" />
+          </button>
+        </div>
+  
+        <div class="ImageStory"><!--Story "Frage"--></div>
+        <div class="d-flex Nova justify-content-end">
+          <img
+            src="/assets/nova/nova_intro_rechts.png"
+            width="100"
+            height="100"
+            id="NovaImage"
+          />
+        </div>
+        
+    </div>
+    
+    `,
+    styleUrls: ['./questions.component.css']
+  })
+  export class StoriesComponent implements DoCheck {
+  
+  @Input() Fragenliste;
+  @Input() Storyliste;
+  i:number=0;
+  test2:string;
+  imgArray= [];
+  fragen:string[]=[]; 
+  fragenanzeige: string;
+  story:string[]=[]; 
+  storyanzeige: string;
+  
+  
+   
+  constructor() { 
+   this.imgArray= [];
+  
+
+    this.imgArray[0]="/assets/fragenkatalog/story_shopping.png";
+  
+    this.imgArray[1] ="/assets/fragenkatalog/story_bahnhof.png";
+
+    this.imgArray[2] ="/assets/fragenkatalog/rechtliche_Hilfe.png";
+  
+    this.imgArray[3] ="/assets/fragenkatalog/wunsch.png";
+
+    this.imgArray[4] ="/assets/fragenkatalog/immobilien_sparen.png";
+
+    this.imgArray[5] ="/assets/fragenkatalog/reisen.png";
+   
 
 
-  constructor() { }
-
-  ngOnInit(): void {
+  
+   
   }
-
-}
+   
+  
+    ngDoCheck(): void {
+      this.fragen = this.Fragenliste;
+      this.story = this.Storyliste;
+      this.storyanzeige=this.story[this.i]
+      
+    }
+  
+  
+   
+    push(){
+      if(this.fragen.length>this.i){
+      this.fragenanzeige=this.fragen[this.i]
+      }
+  
+  
+      if(this.story.length>this.i){
+        this.storyanzeige=this.story[this.i]
+        }
+  
+        this.i=this.i+1;
+       
+  
+    }
+  
+  
+    zurueck(){
+      if(this.fragen.length>this.i){
+        this.fragenanzeige=this.fragen[this.i-2]
+        this.i=this.i-1;
+        }
+  
+    }
+  
+  }
+  
+  
