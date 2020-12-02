c060a540d229bf90c0c4fe5ae6ce6691d886c9f5
tobiasb201 <49474146+tobiasb201@users.noreply.github.com>
2020-12-02 08:46:10 +0100

    Fomattierung

--- Alleare/src/app/quiz/evaluation.component.ts
+++ Alleare/src/app/quiz/evaluation.component.ts
@@ -9,46 +9,57 @@ import { Data } from '@angular/router';
 @Component({
   selector: 'evaluation-quiz',
   template: `
-<br>
-<br>
-<br>
-{{punkte}} Punkte
-    `,
+    <br />
+    <br />
+    <br />
+    {{ punkte }} Punkte
+  `,
   styleUrls: ['./quiz.component.css'],
 })
-export class evaluationComponent implements OnInit{
-  dbpush=firebase.firestore().collection('Benutzer').doc(localStorage.getItem('hans')).collection('Quiz');
-  dbantworten=firebase.firestore().collection('Benutzer').doc(localStorage.getItem('hans')).collection('Quiz');
-  antworten=[];
-  richtigeantworten=[];
-  punkte:number=0;
-  antwortenid=[];
-  dbrichtig=firebase.firestore().collection('Quiz');
+export class evaluationComponent implements OnInit {
+  dbpush = firebase
+    .firestore()
+    .collection('Benutzer')
+    .doc(localStorage.getItem('hans'))
+    .collection('Quiz');
+  dbantworten = firebase
+    .firestore()
+    .collection('Benutzer')
+    .doc(localStorage.getItem('hans'))
+    .collection('Quiz');
+  antworten = [];
+  richtigeantworten = [];
+  punkte: number = 0;
+  antwortenid = [];
+  dbrichtig = firebase.firestore().collection('Quiz');
 
-  constructor(private dataservice: DataService) {
+  constructor(private dataservice: DataService) {}
 
-  }
-
-ngOnInit(){
-    this.dbantworten.get().then((querysnapshot)=>{
-        querysnapshot.forEach((doc)=>{
+  ngOnInit() {
+    this.dbantworten
+      .get()
+      .then((querysnapshot) => {
+        querysnapshot.forEach((doc) => {
           this.antwortenid.push(doc.id);
           this.antworten.push(doc.data().antwort);
-        })
-      }).then(()=>{
-        for(let y=0;y<this.antwortenid.length;y++){
-            this.dbrichtig.doc(this.antwortenid[y]).get().then((doc)=>{
-                this.richtigeantworten.push(doc.data().richtig)
-            })
-        }
+        });
       })
-      setTimeout(() => {
-        for(let i=0;i<this.antwortenid.length;i++){
-            if(this.antworten[i]==this.richtigeantworten[i]){
-              this.punkte=this.punkte+1;
-            }
-          }
-      }, 1300); //Muss noch überarbeitet werden.
-      
-    }
+      .then(() => {
+        for (let y = 0; y < this.antwortenid.length; y++) {
+          this.dbrichtig
+            .doc(this.antwortenid[y])
+            .get()
+            .then((doc) => {
+              this.richtigeantworten.push(doc.data().richtig);
+            });
+        }
+      });
+    setTimeout(() => {
+      for (let i = 0; i < this.antwortenid.length; i++) {
+        if (this.antworten[i] == this.richtigeantworten[i]) {
+          this.punkte = this.punkte + 1;
+        }
+      }
+    }, 1300); //Muss noch überarbeitet werden.
+  }
 }
--- Alleare/src/app/quiz/quiz.component.ts
+++ Alleare/src/app/quiz/quiz.component.ts
@@ -1,59 +1,75 @@
-import { Component, DoCheck, OnInit, ɵallowSanitizationBypassAndThrow } from '@angular/core';
+import {
+  Component,
+  DoCheck,
+  OnInit,
+  ɵallowSanitizationBypassAndThrow,
+} from '@angular/core';
 import firebase from 'firebase';
 import { DataService } from '../services/data.service';
 
 @Component({
   selector: 'app-quiz',
   templateUrl: './quiz.component.html',
-  styleUrls: ['./quiz.component.css']
+  styleUrls: ['./quiz.component.css'],
 })
 export class QuizComponent implements OnInit, DoCheck {
-  switch : boolean = false;
-  routingindex;// muss von den templates hochgesetzt werden.
-  anzeige:string;
-  reihenfolge=["vier","vier","zwei","vier","zwei","zwei","vier","zwei"]
-  anzeigevier:boolean=false;
-  anzeigezwei:boolean=false;
-  auswertung=false;
-  dbantworten=firebase.firestore().collection('Benutzer').doc(localStorage.getItem('hans')).collection('Quiz');
-  antworten=[];
-  richtigeantworten=[];
-  punkte:number=0;
-  antwortenid=[];
-  jalla=true;
-  dbrichtig=firebase.firestore().collection('Quiz');
+  switch: boolean = false;
+  routingindex; // muss von den templates hochgesetzt werden.
+  anzeige: string;
+  reihenfolge = [
+    'vier',
+    'vier',
+    'zwei',
+    'vier',
+    'zwei',
+    'zwei',
+    'vier',
+    'zwei',
+  ];
+  anzeigevier: boolean = false;
+  anzeigezwei: boolean = false;
+  auswertung = false;
+  dbantworten = firebase
+    .firestore()
+    .collection('Benutzer')
+    .doc(localStorage.getItem('hans'))
+    .collection('Quiz');
+  antworten = [];
+  richtigeantworten = [];
+  punkte: number = 0;
+  antwortenid = [];
+  jalla = true;
+  dbrichtig = firebase.firestore().collection('Quiz');
 
-  constructor(private dataservice:DataService) { }
+  constructor(private dataservice: DataService) {}
 
-  ngOnInit(){
+  ngOnInit() {
     this.dataservice.addquizrouting(0);
     this.dataservice.addindexspeichernzwei(0);
     this.dataservice.addindexspeichernvier(0);
 
-    this.dbantworten.get().then((querysnapshot)=>{//Löscht Benutzerantworten, von davor
-      querysnapshot.forEach((doc)=>{
+    this.dbantworten.get().then((querysnapshot) => {
+      //Löscht Benutzerantworten, von davor
+      querysnapshot.forEach((doc) => {
         doc.ref.delete();
-        })
-      })
-
+      });
+    });
   }
 
-  ngDoCheck(){
-    this.routingindex=this.dataservice.getquizrouting();
-    if(this.reihenfolge[this.routingindex]=='vier'){
-      this.anzeigezwei=false;
-      this.anzeigevier=true; 
+  ngDoCheck() {
+    this.routingindex = this.dataservice.getquizrouting();
+    if (this.reihenfolge[this.routingindex] == 'vier') {
+      this.anzeigezwei = false;
+      this.anzeigevier = true;
     }
-    if(this.reihenfolge[this.routingindex]=='zwei'){
-      this.anzeigezwei=true;
-      this.anzeigevier=false;
+    if (this.reihenfolge[this.routingindex] == 'zwei') {
+      this.anzeigezwei = true;
+      this.anzeigevier = false;
     }
-    if(this.routingindex>=this.reihenfolge.length){
-      this.anzeigezwei=false;
-      this.anzeigevier=false;
-      this.auswertung=true;
+    if (this.routingindex >= this.reihenfolge.length) {
+      this.anzeigezwei = false;
+      this.anzeigevier = false;
+      this.auswertung = true;
     }
   }
-
 }
-
