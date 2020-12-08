6a39b04d6d6a3869a1ca607ea9a2691a459cc1d1
DannySenpai <58006632+DannySenpai@users.noreply.github.com>
2020-12-08 12:59:18 +0100

    Intro -wiederholt sich nicht mehr

--- Alleare/src/app/header/header.component.ts
+++ Alleare/src/app/header/header.component.ts
@@ -1,15 +1,23 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, DoCheck, OnInit } from '@angular/core';
+import { HomeComponent } from '../home/home.component';
+import { DataService } from '../services/data.service';
+
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.css']
+  styleUrls: ['./header.component.css'],
 })
-export class HeaderComponent implements OnInit {
+export class HeaderComponent implements DoCheck {
+  grau = true;
 
-  constructor() { }
+  constructor(private DataService: DataService) {}
 
-  ngOnInit(): void {
+  ngDoCheck(){
+    if(this.grau == true){
+      
+    }
+    this.grau = this.DataService.getHeader();
   }
-
+  
 }
--- Alleare/src/app/intro/insurance.component.ts
+++ Alleare/src/app/intro/insurance.component.ts
@@ -4,83 +4,133 @@ import { DataService } from '../services/data.service';
 @Component({
   selector: 'app-insurance',
   template: `
+    <head>
+      <!-- Required meta tags -->
+      <meta charset="utf-8" />
+      <meta
+        name="viewport"
+        content="width=device-width, initial-scale=1, shrink-to-fit=no"
+      />
+
+      <!-- Bootstrap CSS -->
+      <link
+        rel="stylesheet"
+        href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css"
+        integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2"
+        crossorigin="anonymous"
+      />
+    </head>
+
     <body>
       <!--Screen Intro Irrtümer Anfang-->
       <div [hidden]="eins">
-        <div id="HaftpflichtEinblenden">
-          <img
-            class="HaftpflichtBild"
-            src="/assets/Bilder_Irrtuemer_Altagssituationen/Haftpflicht.png"
-          />
-          <div class="TextHaftpflicht">
-            <p>
-              <b>Haftpflicht zahlt den <br />Neupreis</b>
-            </p>
+        <div class="dialog1 container">
+          <div class="row">
+            <div class="col col-12">
+              <div id="HaftpflichtEinblenden">
+                <img
+                  class="HaftpflichtBild img-fluid"
+                  src="/assets/Bilder_Irrtuemer_Altagssituationen/Haftpflicht.png"
+                />
+                <div class="TextHaftpflicht">
+                  <p>
+                    <b>Haftpflicht zahlt den <br />Neupreis</b>
+                  </p>
+                </div>
+              </div>
+            </div>
           </div>
-        </div>
 
-        <div id="HausratEinblenden">
-          <img
-            class="HausratBild"
-            src="/assets/Bilder_Irrtuemer_Altagssituationen/Hausrat.png"
-          />
-          <div class="TextHausrat">
-            <p>
-              <b>Hausrat zahlt immer bei <br />Raub</b>
-            </p>
+          <div class="row">
+            <div class="col col-10">
+              <div id="HausratEinblenden">
+                <img
+                  class="HausratBild img-fluid"
+                  src="/assets/Bilder_Irrtuemer_Altagssituationen/Hausrat.png"
+                />
+                <div class="TextHausrat">
+                  <p>
+                    <b>Hausrat zahlt immer bei <br />Raub</b>
+                  </p>
+                </div>
+              </div>
+            </div>
           </div>
         </div>
       </div>
       <!--Screen Intro Irrtümer Ende-->
+
       <div [hidden]="zwei">
         <!--Screen Intro Altagssituation Autounfall Anfang-->
-        <div id="PersonenbefoerderungEinblenden">
-          <img
-            class="PersonenbefoerderungBild"
-            src="/assets/Bilder_Irrtuemer_Altagssituationen/header-personenbefoerderung.png"
-          />
-          <div class="TextPersonenbefoerderung">
-            <p>
-              <b>Unfall mit dem Auto <br />meines Freundes</b>
-            </p>
-          </div>
-        </div>
 
-        <div id="AutoschadenEinblenden">
-          <img
-            class="AutoschadenBild"
-            src="/assets/Bilder_Irrtuemer_Altagssituationen/Autoschaden.png"
-          />
-          <div class="TextAutoschaden">
-            <p><b>Wer trägt die Kosten?</b></p>
+        <div class="dialog2 container">
+          <div class="row">
+            <div class="col col-12">
+              <div id="PersonenbefoerderungEinblenden">
+                <img
+                  class="PersonenbefoerderungBild img-fluid"
+                  src="/assets/Bilder_Irrtuemer_Altagssituationen/header-personenbefoerderung.png"
+                />
+                <div class="TextPersonenbefoerderung">
+                  <p>
+                    <b>Unfall mit dem Auto <br />meines Freundes</b>
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div class="row">
+            <div class="col col-10">
+              <div id="AutoschadenEinblenden">
+                <img
+                  class="AutoschadenBild img-fluid"
+                  src="/assets/Bilder_Irrtuemer_Altagssituationen/Autoschaden.png"
+                />
+                <div class="TextAutoschaden">
+                  <p><b>Wer trägt die Kosten?</b></p>
+                </div>
+              </div>
+            </div>
           </div>
         </div>
       </div>
       <!--Screen Intro Altagssituation Autounfall Ende-->
       <div [hidden]="drei">
         <!-- Screen Intro Altagssituation Private Unfallversicherung Anfang-->
-        <div id="PrivateUnfallversicherungEinblenden">
-          <img
-            class="PrivateUnfallversicherungBild"
-            src="/assets/Bilder_Irrtuemer_Altagssituationen/Private_Unfallversicherung.png"
-          />
-          <div class="TextPrivateUnfallversicherung">
-            <p><b>Unfall im eigenen Haus</b></p>
-          </div>
-        </div>
 
-        <div id="HaushaltsunfallEinblenden">
-          <img
-            class="HaushaltsunfallBild"
-            src="/assets/Bilder_Irrtuemer_Altagssituationen/haushaltsunfall.png"
-          />
-          <div class="TextHaushaltsunfall">
-            <p>
-              <b
-                >Wer zahlt die <br />
-                Behandlungskosten?</b
-              >
-            </p>
+        <div class="dialog3 Container">
+          <div class="row">
+            <div class="col col-12">
+              <div id="PrivateUnfallversicherungEinblenden">
+                <img
+                  class="PrivateUnfallversicherungBild img-fluid"
+                  src="/assets/Bilder_Irrtuemer_Altagssituationen/Private_Unfallversicherung.png"
+                />
+                <div class="TextPrivateUnfallversicherung">
+                  <p><b>Unfall im eigenen Haus</b></p>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div class="row">
+            <div class="col col-10">
+              <div id="HaushaltsunfallEinblenden">
+                <img
+                  class="HaushaltsunfallBild img-fluid"
+                  src="/assets/Bilder_Irrtuemer_Altagssituationen/haushaltsunfall.png"
+                />
+                <div class="TextHaushaltsunfall">
+                  <p>
+                    <b
+                      >Wer zahlt die <br />
+                      Behandlungskosten?</b
+                    >
+                  </p>
+                </div>
+              </div>
+            </div>
           </div>
         </div>
       </div>
