194936329757c1c879d45597b743e64c918c0c51
Zapa-Mike <zapa.mike@yahoo.com>
2020-12-14 13:10:00 +0100

    Namenseingabe eingeschräankt -auf 20 zeichen

--- Alleare/src/app/intro/intro.component.ts
+++ Alleare/src/app/intro/intro.component.ts
@@ -1,10 +1,10 @@
-import { LocationStrategy } from '@angular/common';
-import { isGeneratedFile } from '@angular/compiler/src/aot/util';
-import { Component, OnInit, Input, DoCheck } from '@angular/core';
-import { FormControl, FormGroup } from '@angular/forms';
+import { Component, OnInit, DoCheck } from '@angular/core';
+import {
+  FormBuilder,
+  Validators,
+} from '@angular/forms';
 import { Router } from '@angular/router';
 import firebase from 'firebase';
-import { stringify } from 'querystring';
 import { DataService } from '../services/data.service';
 
 @Component({
@@ -12,88 +12,15 @@ import { DataService } from '../services/data.service';
   templateUrl: './intro.component.html',
   styleUrls: ['./intro.component.css'],
 })
-/*export class IntroComponent implements DoCheck, OnInit {
-  form: FormGroup;
-  public Name: string;
-  homeintro = true;
-  //Routing
-  logo: boolean = true;
-  namenseingabe: boolean = false;
-  novaIntro: boolean = false;
-  novadialog: boolean = false;
-  indexnovadialog: number;
-  insurance: boolean = false;
-  docRef = firebase
-  .firestore()
-  .collection('Benutzer')
-  .doc(localStorage.getItem('hans'));
-
-  constructor(private router:Router, private dataservice: DataService) {
-    setTimeout(() => {
-      this.docRef.get().then((doc) => {
-        this.homeintro  = doc.data().homeintro;
-        if(this.homeintro!=false){
-          this.docRef.update({homeintro:true})
-          this.homeintro=true
-        }})
-
-      this.logo = false;
-      console.log(this.homeintro);
-      if(this.homeintro==true){
-        this.namenseingabe = true;
-
-      }else if (this.homeintro==false){
-this.router.navigate(['/home'])
-      }
-      
-    }, 2888);
-    this.form = new FormGroup({
-      Name: new FormControl(),
-    });
-
-    this.dataservice.getIndexdialog();
-    this.dataservice.currentIndex2.subscribe(
-      (currentIndex2) => (this.indexnovadialog = currentIndex2)
-    );
-
-    this.novadialog= false;
-    this.insurance= false;
-    this.namenseingabe=false;
-    this.novaIntro=false;
-  }
-  ngOnInit() {
-    
-     
-  }
-
-  ngDoCheck() {
-    if (this.indexnovadialog == 1 && this.homeintro == true) {
-      this.insurance = false;
-      this.novadialog = true;
-    }
-  }
-
-  saveName() {
-    this.Name = this.form.value.Name;
-    firebase
-      .firestore()
-      .collection('Benutzer')
-      .doc(localStorage.getItem('hans'))
-      .set({
-        Name: this.Name,
-        homeintro: true,
-      });
-    this.namenseingabe = false;
-    this.novaIntro = true;
-  }
-  //Routing
-  weiter() {
-    this.novaIntro = false;
-    this.insurance = true;
-  }
-}*/
 export class IntroComponent implements OnInit, DoCheck {
-  form: FormGroup;
+  public form = this.fb.group({
+    Name: [
+      '',
+      {
+        validators: [Validators.maxLength(20)],
+      },
+    ],
+  });
   public Name: string;
   homeintro = true;
   //Routing
@@ -104,14 +31,15 @@ export class IntroComponent implements OnInit, DoCheck {
   indexnovadialog: number;
   insurance: boolean = false;
   docRef = firebase
-  .firestore()
-  .collection('Benutzer')
-  .doc(localStorage.getItem('hans'));
+    .firestore()
+    .collection('Benutzer')
+    .doc(localStorage.getItem('hans'));
 
-  constructor(private router:Router, private dataservice: DataService) {
-    this.form = new FormGroup({
-      Name: new FormControl(),
-    });
+  constructor(
+    private router: Router,
+    private dataservice: DataService,
+    private fb: FormBuilder
+  ) {
     this.dataservice.getIndexdialog();
     this.dataservice.currentIndex2.subscribe(
       (currentIndex2) => (this.indexnovadialog = currentIndex2)
@@ -127,38 +55,34 @@ export class IntroComponent implements OnInit, DoCheck {
 
   ngOnInit() {
     this.logo = true;
-    this.novadialog= false;
-    this.insurance= false;
-    this.namenseingabe=false;
-    this.novaIntro=false;
-   
+    this.novadialog = false;
+    this.insurance = false;
+    this.namenseingabe = false;
+    this.novaIntro = false;
+
     this.docRef.get().then((doc) => {
-     
       if (doc.exists) {
-        this.homeintro  = doc.data().homeintro;
-        if(this.homeintro!=false){
-          this.docRef.update({homeintro:true})
-          this.homeintro=true
+        this.homeintro = doc.data().homeintro;
+        if (this.homeintro != false) {
+          this.docRef.update({ homeintro: true });
+          this.homeintro = true;
         }
       }
-      console.log(this.homeintro)
+      console.log(this.homeintro);
       setTimeout(() => {
         this.logo = false;
-        
-        if(this.homeintro==true){
+
+        if (this.homeintro == true) {
           this.namenseingabe = true;
-  
-        }else if (this.homeintro==false){
-  this.router.navigate(['/home'])
+        } else if (this.homeintro == false) {
+          this.router.navigate(['/home']);
         }
-        
       }, 2888);
-    })
-    
+    });
   }
 
   saveName() {
-    this.Name = this.form.value.Name;
+    this.Name = this.form.get('Name').value;
     firebase
       .firestore()
       .collection('Benutzer')
